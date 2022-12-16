adbdcac56b34c50da9559245b8977ba93a7d9219
Alev Sametoglu <alevsametoglu.fr@gmail.com>
2022-12-16 13:21:44 +0100

    cerate item component

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons-vue";
+import { Row, Col } from "ant-design-vue";
+
+defineProps({
+  name: String,
+  price: Number,
+  city: String,
+  refNumber: String,
+});
+
+const emit = defineEmits(["delete", "update"]);
+</script>
+
+<template>
+  <a-card hoverable>
+    <template #actions>
+      <delete-outlined
+        key="delete"
+        style="color: red"
+        @click="emit('delete')"
+      />
+      <edit-outlined key="edit" style="color: blue" @click="emit('update')" />
+    </template>
+    <a-card-meta>
+      <template #title>
+        <Row>
+          <Col span="18">{{ name }}</Col>
+          <Col span="6" style="text-align: right"
+            >{{ price }} <span>&#8364;</span></Col
+          >
+        </Row>
+      </template>
+      <template #description>
+        <Row>
+          <Col span="14">{{ city }}</Col>
+          <Col span="10" style="text-align: right"> ref:{{ refNumber }}</Col>
+        </Row>
+      </template>
+    </a-card-meta>
+  </a-card>
+</template>

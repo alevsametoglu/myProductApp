7627f26de465e4f0e87061ca2401ad75119f2466
Alev Sametoglu <alevsametoglu.fr@gmail.com>
2022-12-16 13:35:13 +0100

    cerate search component

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from "vue";
+//declared search query param with ref
+const searchQuery = ref({
+  type: "or",
+  searchKey: "",
+});
+const emit = defineEmits(["change"]);
+
+function handleSearchKey(value) {
+  searchQuery.value.searchKey = value;
+  emit("change", searchQuery.value);
+}
+function handleSearchType(value) {
+  searchQuery.value.type = value;
+  emit("change", searchQuery.value);
+}
+</script>
+
+<template>
+  <a-input-group
+    compact
+    style="padding: 20px; display: flex; align-items: center"
+  >
+    <a-select
+      :value="searchQuery.type"
+      @change="handleSearchType"
+      style="min-width: 70px"
+    >
+      <a-select-option value="or">Or</a-select-option>
+      <a-select-option value="and">And</a-select-option>
+    </a-select>
+    <a-input-search
+      placeholder="Search product by name or ref"
+      enter-button
+      @search="handleSearchKey"
+    />
+  </a-input-group>
+</template>

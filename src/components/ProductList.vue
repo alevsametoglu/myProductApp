67aed643fe94d13f51a8499adf4363e541e9bd08
Alev Sametoglu <alevsametoglu.fr@gmail.com>
2022-12-16 13:23:02 +0100

    create product list component

---
+++ src/components/ProductList.vue
@@ -0,0 +1,38 @@
+<script setup>
+import ProductItem from "./ProductItem.vue";
+
+defineProps({ products: Array });
+const emit = defineEmits(["deleteProduct", "updateProduct"]);
+</script>
+
+<template>
+  <div class="searchBar">
+    <span> </span>
+  </div>
+  <div>
+    <ul class="productList">
+      <li v-for="product in products" :key="product.refNumber">
+        <ProductItem
+          :name="product.name"
+          :price="product.price"
+          :city="product.city"
+          :refNumber="product.refNumber"
+          @delete="() => emit('deleteProduct', product.refNumber)"
+          @update="() => emit('updateProduct', product.refNumber)"
+        />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.productList {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  gap: 40px;
+  padding: 10px;
+  li {
+    list-style-type: none;
+  }
+}
+</style>
